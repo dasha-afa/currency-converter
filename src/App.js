13a69b5b38ce95550f241b8c396739408328722a
аша фанасьєва <dashaafa01102004@gmail.com>
2023-10-07 23:52:27 +0300

    Applying change button and style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import { useEffect } from 'react';
+import svgIcon from './icons/exchange-svgrepo-com.svg'
 
 function App() {
   const [value, setValue] = useState(1);
@@ -27,24 +28,32 @@ function App() {
     setTo(e.value)
   }
 
-  const fetchData = () => {
+  const changeCurrencyType = () => {
+    setFrom(to)
+    setTo(from)
+  }
+
+  const fetchData = useCallback(() => {
     fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
     .then(response => response.json())
     .then(data => setInformation(data[from]))
-  }
+  }, [from])
 
   useEffect(() => {
-    fetchData()
-    setVariants(Object.keys(information))
-  }, [information])
+    fetchData();
+    setVariants(Object.keys(information));
+  }, [fetchData, information])
 
   return (
     <div className="App">
        <h1>Convert your currency fast!</h1>
 
+       <div className='container'>
+
        <div className='input_and_choose_currency'>
+
           <form>
-            <label for="input_currency">Write a number in {upperCase(from)}</label>
+            <label htmlFor="input_currency">Write a number in {upperCase(from)}</label>
             <input
             id='input_currency'
             value={value}
@@ -53,18 +62,29 @@ function App() {
             </input>
           </form>
 
-          <Dropdown options={variants} onChange={handleFrom} value={from}/>
+          <div className='convert_to'>
+          <label htmlFor="to_currency">Convert to</label>
+          <Dropdown id='to_currency' options={variants} onChange={handleTo} value={to}/>
+          </div>
 
           <div className='resault'>
             <p>Resault:</p>
             <p>{value === '' ? 0 : value} {upperCase(from)} = {value * information[to]} {upperCase(to)}</p>
           </div>
-
-          <Dropdown options={variants} onChange={handleTo} value={to}/>
-       
+          
+          <div className='convert_from'>
+          <label htmlFor="from_currency">Convert from</label>
+          <Dropdown id='from_currency' options={variants} onChange={handleFrom} value={from}/>
+          </div>
+          
         </div>
-    </div>
-  );
-}
+
+        <div className='buttonChange'>
+            <img onClick={changeCurrencyType} src={svgIcon} alt='Exchange button icon'></img>
+        </div>
+
+      </div>
+  </div>
+);}
 
 export default App;
